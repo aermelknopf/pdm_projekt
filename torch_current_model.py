a804fb04efa99359ff444599f0b29403b7dcca47
Benjamin-Philipp Roth <B.Roth@cluetec.de>
2022-04-28 16:17:57 +0200

    added documentation

--- torch_current_model.py
+++ torch_current_model.py
@@ -1,3 +1,6 @@
+# This file contains the last used LSTM models.
+# It is used to reduce code changes in the torch_lstm_experiment.py file for clarity reasons. '''
+
 import torch
 import torch.nn as nn
 from torch.nn import Linear
@@ -5,7 +8,10 @@ from torch.nn import Linear
 import torch_custom_lstm as custom
 import custom_lstm_example as reference
 
+
 class SliceModel(nn.Module):
+    """ Current version of the SlicedModel tested in torch_lstm_experiment.py """
+
     def __init__(self, nb_out):
         super().__init__()
         self.sliceLSTM1 = custom.SliceLSTM([(11, 1), (14, 1)])
@@ -26,27 +32,10 @@ class SliceModel(nn.Module):
         return x
 
 
-class LibraryModel(nn.Module):
-    def __init__(self, nb_out, lstm_feat_size, lstm_hid_size, lstm_layers=2, lstm_batch_first=True):
-        super().__init__()
-        self.LSTM = torch.nn.LSTM(input_size=lstm_feat_size, hidden_size=lstm_hid_size, num_layers=lstm_layers,
-                                  batch_first=lstm_batch_first, dropout=0.2)
-        self.out = Linear(lstm_hid_size, nb_out)
-        self.out_activation = torch.nn.Sigmoid()
-
-    def forward(self, x):
-        # print(f"input: {x.shape}")
-        x, _ = self.LSTM(x)
-        x = x[:, -1, :]    # only take last output -> "return_sequence = False"
-        # print(f"lstm_output: {x.shape}")
-        x = self.out(x)
-        # print(f"linear output: {x.shape}")
-        x = self.out_activation(x)
-        # print(f"output: {x.shape}")
-        return x
-
 
 class ReferenceCustomModel(nn.Module):
+    """ Current version of the model used as comparison to SlicedModel in torch_lstm_experiment.py """
+
     def __init__(self, nb_out):
         super().__init__()
         self.LSTM1 = reference.CustomLSTM(25, 5)
@@ -63,6 +52,28 @@ class ReferenceCustomModel(nn.Module):
         # x, _ = self.LSTM2(x)
         x = x[:, -1, :]  # only take last output -> "return_sequence = False"
         # x = self.DropOut2(x)
+        # print(f"lstm_output: {x.shape}")
+        x = self.out(x)
+        # print(f"linear output: {x.shape}")
+        x = self.out_activation(x)
+        # print(f"output: {x.shape}")
+        return x
+
+
+class LibraryModel(nn.Module):
+    """ Current version of a fully integrated pytorch library LSTM model for ballpark comparison """
+
+    def __init__(self, nb_out, lstm_feat_size, lstm_hid_size, lstm_layers=2, lstm_batch_first=True):
+        super().__init__()
+        self.LSTM = torch.nn.LSTM(input_size=lstm_feat_size, hidden_size=lstm_hid_size, num_layers=lstm_layers,
+                                  batch_first=lstm_batch_first, dropout=0.2)
+        self.out = Linear(lstm_hid_size, nb_out)
+        self.out_activation = torch.nn.Sigmoid()
+
+    def forward(self, x):
+        # print(f"input: {x.shape}")
+        x, _ = self.LSTM(x)
+        x = x[:, -1, :]  # only take last output -> "return_sequence = False"
         # print(f"lstm_output: {x.shape}")
         x = self.out(x)
         # print(f"linear output: {x.shape}")
--- torch_custom_lstm.py
+++ torch_custom_lstm.py
@@ -1,24 +1,38 @@
 # Slice LSTM implementation
-# inspired by example from towardsdatascience.com
-# see: custom_lstm.example.py
+# inspired by example from https://towardsdatascience.com/building-a-lstm-by-hand-on-pytorch-59c02a4ec091
+# The LSTM-Cell uses the optimization of calculating the weights of all four LSTM gates in one big matrix
+# (for input and hidden state units respective). Since gates are sliced, this results in a list of matrices.
 
 import math
-
 import torch
 import torch.nn as nn
 
 
-"""FINAL"""
 class SliceLSTM(nn.Module):
-    def __init__(self, lstm_slices):
+    """ Experimental new version of an LSTM layer, in which data slices of the input are processed by 'slices'
+        of the typical LSTM gates (input gate i, forward gate f, g, output gate o) and TODO: complete """
+
+    def __init__(self, lstm_slices: list[tuple]):
         super().__init__()
+        # length of the input slices
         self.input_slices = [x[0] for x in lstm_slices]
+        # length of the hidden units of the respective input slices (index-paired with self.input_slices)
         self.hidden_slices = [x[1] for x in lstm_slices]
+        # hidden size of the entire layer
         self.hidden_size = sum(self.hidden_slices)
+        # list of matrices of weights for input unit slices. Concatenation of weight matrix of all four gates
+        # (i, f, g, o). One matrix contains weights for one slice of  i, f, g and o.
         self.Ws = nn.ParameterList([nn.Parameter(torch.Tensor(input_size, hidden_size * 4)) for input_size, hidden_size in lstm_slices])
+        # list of matrices of weights for hidden unit slices. Concatenation of weight matrix of all four gates
+        # (i, f, g, o). One matrix contains weights for one slice of  i, f, g and o.
         self.Us = nn.ParameterList([nn.Parameter(torch.Tensor(hidden_size, hidden_size * 4)) for _, hidden_size in lstm_slices])
+        # list of bias vectors for each slice. One vector contains concatenated biases for all four gates i, f, g and o.
         self.biases = nn.ParameterList([nn.Parameter(torch.Tensor(hidden_size * 4)) for _, hidden_size in lstm_slices])
+        # weight matrix for the dense connector layer connecting the gate slices. Contains concatenated weights for all
+        # four gates i, f, g and o.
         self.connector_Ws = nn.Parameter(torch.Tensor(self.hidden_size, self.hidden_size * 4))
+        # bias vector for the dense connector layer connecting the gate slices. Contains concatenated biases for all
+        # four gates i, f, g and o.
         self.connector_biases = nn.Parameter(torch.Tensor(self.hidden_size * 4))
         self.init_weights()
 
@@ -30,18 +44,17 @@ class SliceLSTM(nn.Module):
     def forward(self, x,
                 init_states=None):
 
-        # Do some input dimension sanity checks:
+        # Some input dimension sanity checks:
         batch_size, sequence_length, feature_size = x.size()
         # print(f"input shape: {x.size()}")
         # print(f"batch size: {batch_size}, sequence_length: {sequence_length}, feature size: {feature_size}")
         total_input_size = sum(self.input_slices)
         assert(feature_size == total_input_size)
 
-
-        """Assumes x is of shape (batch, sequence, feature)"""
+        # assumes x is of shape (batch, sequence, feature) !!!!
         bs, seq_sz, _ = x.size()
         hidden_seq = []
-        # TODO: revisit this later once you know what you are doing
+        # TODO: other initialization possible
         if init_states is None:
             h_t, c_t = (torch.zeros(bs, self.hidden_size).to(x.device),
                         torch.zeros(bs, self.hidden_size).to(x.device))
@@ -54,22 +67,21 @@ class SliceLSTM(nn.Module):
             slice_is, slice_fs, slice_gs, slice_os = [], [], [], []
 
             for index, (input_size, hidden_size) in enumerate(zip(self.input_slices, self.hidden_slices)):
-                in_end = in_start + input_size
-                hid_end = hid_start + hidden_size
+                in_end = in_start + input_size          # end index exclusive in slicing
+                hid_end = hid_start + hidden_size       # end index exclusive in slicing
 
-                cur_x_t = x[:, t, in_start:in_end]
-                cur_h_t = h_t[:, hid_start:hid_end]
+                cur_x_t = x[:, t, in_start:in_end]      # input units for current slice (for entire batch)
+                cur_h_t = h_t[:, hid_start:hid_end]     # hidden units for current slice (batch independent)
 
-                # batch the computations for each slice into a single matrix multiplication
-                test = cur_h_t @ self.Us[index]
+                # batch the computations for each slice into one single matrix multiplication
                 gates = cur_x_t @ self.Ws[index] + cur_h_t @ self.Us[index] + self.biases[index]
 
-                # apply activation functions for each split gate
+                # apply activation functions for each gate slice
                 i_t, f_t, g_t, o_t = (
-                    torch.sigmoid(gates[:, :hidden_size]),  # input
-                    torch.sigmoid(gates[:, hidden_size : hidden_size * 2]),  # forget
-                    torch.tanh(gates[:, hidden_size * 2 : hidden_size * 3]),
-                    torch.sigmoid(gates[:, hidden_size * 3 :]),  # output
+                    torch.sigmoid(gates[:, :hidden_size]),                     # input gate slice
+                    torch.sigmoid(gates[:, hidden_size: hidden_size * 2]),     # forget gate slice
+                    torch.tanh(gates[:, hidden_size * 2: hidden_size * 3]),    # 'gate gate' slice
+                    torch.sigmoid(gates[:, hidden_size * 3:]),                 # output gate slice
                 )
 
                 slice_is.append(i_t)
@@ -82,7 +94,7 @@ class SliceLSTM(nn.Module):
             total_g = torch.cat(slice_gs, dim=1)
             total_o = torch.cat(slice_os, dim=1)
 
-            # Dense Connector-Layer
+            # apply dense connector-layer
             i_t = torch.sigmoid(total_i @ self.connector_Ws[:, :self.hidden_size]
                                 + self.connector_biases[:self.hidden_size])
             f_t = torch.sigmoid(total_f @ self.connector_Ws[:, self.hidden_size:self.hidden_size * 2]
